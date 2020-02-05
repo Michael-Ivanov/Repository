b77c3ad1a9943190ab3c5412a3200fd5f3a59984
Michael-Ivanov <60621280+Michael-Ivanov@users.noreply.github.com>
2020-02-05 18:03:51 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+   public static void main(final String[] args) {
+      System.out.println("Hello, world!");
+      System.out.println("VIM text editor isn't very easy to learn");
+      System.out.println("Hi again");
+   }
+}
